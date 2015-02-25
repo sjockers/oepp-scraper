630ecfaceb5c284c5d2e22a51ce8ed6a41eb185a
Simon Jockers <hello@simonjockers.com>
2015-02-25 18:16:45 +0100

    Improve async handling of requests using promises.

--- oepp-scraper.js
+++ oepp-scraper.js
@@ -2,36 +2,46 @@
 
 var scraperjs = require('scraperjs');
 var fs = require('fs');
+var Q = require('q');
 
 var baseUrl = 'http://www.ppp-projektdatenbank.de/index.php?id=9&tx_ppp_controller_searchmap[sector]=&tx_ppp_controller_searchmap[subsector]=&tx_ppp_controller_searchmap[state]=&tx_ppp_controller_searchmap[county]=&tx_ppp_controller_searchmap[action][showList]=Suchen&tx_ppp_controller_searchmap[offset]=';
 var out = fs.createWriteStream('data.json', { encoding: "utf8" });
 
-for (i = 0; i < 20; i++) {
+for (i = 0; i < 1; i++) {
   var offset = i * 15;
   var indexUrl = baseUrl + offset;
-  scrapeProjectIndexPage(indexUrl);
+  var scraper = scrapeProjectIndexPage(indexUrl);
+
+  scraper.then(function(data) {
+    data.lastReturn.then(function(sets) {
+      console.log(sets);
+    });
+  });
 }
 
 function scrapeProjectIndexPage (url) {
   var indexScraper = scraperjs.StaticScraper.create(url);
-  indexScraper.scrape(function($) {
-    $("#list td a").map(function() {
+  return indexScraper.scrape(function($) {
+    var details = $("#list td a").map(function() {
       var detailsUrl = $(this).attr('href');
-      scrapeProjectDetailsPage(detailsUrl);
-    });
+      var detailsDeferred = Q.defer();
+      scrapeProjectDetailsPage(detailsUrl).done(function(data) {
+        detailsDeferred.resolve(data.lastReturn);
+      });
+      return detailsDeferred.promise;
+    }).get();
+    return Q.all(details);
   });
 }
 
 function scrapeProjectDetailsPage (url) {
   var detailsScraper = scraperjs.StaticScraper.create(url);
-  detailsScraper.scrape(function($) {
+  return detailsScraper.scrape(function($) {
     return {
       title: $("#content h2").text(),
       source: url,
       meta: extractMetaData($)
     };
-  }, function(item) {
-    out.write(JSON.stringify(item, null, 2));
   });
 }
 
