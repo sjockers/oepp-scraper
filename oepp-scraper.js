2075f71bbe0bddf44219fa2e39efd0c7d5bd1fd3
Simon Jockers <hello@simonjockers.com>
2015-02-25 12:04:04 +0100

    Extract project meta data as key value pairs.

--- oepp-scraper.js
+++ oepp-scraper.js
@@ -6,7 +6,7 @@ var fs = require('fs');
 var baseUrl = 'http://www.ppp-projektdatenbank.de/index.php?id=9&tx_ppp_controller_searchmap[sector]=&tx_ppp_controller_searchmap[subsector]=&tx_ppp_controller_searchmap[state]=&tx_ppp_controller_searchmap[county]=&tx_ppp_controller_searchmap[action][showList]=Suchen&tx_ppp_controller_searchmap[offset]=';
 var out = fs.createWriteStream('data.json', { encoding: "utf8" });
 
-for (i = 0; i < 10; i++) {
+for (i = 0; i < 20; i++) {
   var offset = i * 15;
   var indexUrl = baseUrl + offset;
   scrapeProjectIndexPage(indexUrl);
@@ -24,19 +24,23 @@ function scrapeProjectIndexPage (url) {
 
 function scrapeProjectDetailsPage (url) {
   var detailsScraper = scraperjs.StaticScraper.create(url);
-
   detailsScraper.scrape(function($) {
     return {
       title: $("#content h2").text(),
       source: url,
-      meta: $("td.label").map(function() {
-        return {
-          label: $(this).text().trim(),
-          data: $(this).next().text().trim()
-        };
-      }).get()
+      meta: extractMetaData($)
     };
   }, function(item) {
     out.write(JSON.stringify(item, null, 2));
   });
+}
+
+function extractMetaData ($) {
+  var input = $('h3:contains("Projektstammdaten") + table td.label');
+  var output = {};
+  input.each(function() {
+    var key = $(this).text().trim();
+    output[key] = $(this).next().text().trim();
+  });
+  return output;
 }
