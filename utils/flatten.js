8c1d919eb188f1f8474f8e6f0e38d8b960e4fa88
Simon Jockers <hello@simonjockers.com>
2015-03-03 17:58:56 +0100

    Add contractor data to output.

--- utils/flatten.js
+++ utils/flatten.js
@@ -2,12 +2,16 @@ var _ = require('lodash');
 var investmentValueUtils = require('./investment_volume.js');
 var dateUtils = require('./date.js');
 
+var PROJECT_META = 'Projektstammdaten';
+var CONTRACTOR = 'Ansprechpartner auf Auftragnehmerseite';
+
 function flatten(data) {
   data = investmentVolumeUtils.filterClosedContractsWithInvestmentData(data);
   data = investmentValueUtils.harmonize(data);
   date = dateUtils.harmonize(data);
   return _.map(data, function(item) {
-    _.assign(item, item.data.Projektstammdaten);
+    _.assign(item, item.data[PROJECT_META]);
+    _.assign(item, item.data[CONTRACTOR]);
     delete item.data;
     return item;
   });
